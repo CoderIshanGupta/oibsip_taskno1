e97e7224a78c592bb2f17460decbd52b493203d6
CoderIshanGupta <guptaishan2609@gmail.com>
2025-05-04 22:40:10 +0530

    Create unit.dart

---
+++ lib/models/unit.dart
@@ -0,0 +1,11 @@
+class Unit {
+  final String name;
+  final String symbol;
+  final double conversionFactor;
+
+  Unit({
+    required this.name,
+    required this.symbol,
+    required this.conversionFactor,
+  });
+}
